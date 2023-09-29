ea1a57366dfd60b6d6ac0999275dbc7c956a6028
Marcus Sanchez <marcussanchez031@gmail.com>
2023-09-28 20:31:50 -0400

    part 2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, redirect, render_template, request
-from models import db, connect_db, Users
+from models import db, connect_db, Users, Posts
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/blogly'
@@ -21,19 +21,20 @@ def redirect_to_users():
     return redirect('/users')
 
 
+# USERS ROUTES
 @app.route('/users')
 def render_users():
     users = Users.query.order_by(Users.last_name, Users.first_name).all()
-    return render_template('users.jinja2', users=users)
+    return render_template('users/users.jinja2', users=users)
 
 
 @app.route('/users/new')
-def users_new_form():
-    return render_template('new.jinja2')
+def render_new():
+    return render_template('users/new.jinja2')
 
 
 @app.route("/users/new", methods=["POST"])
-def users_new():
+def create_new_user():
     try:
         fn = request.form['first_name']
         ln = request.form['last_name']
@@ -55,25 +56,25 @@ def users_new():
 
 
 @app.route('/users/<int:user_id>')
-def users_show(user_id):
+def render_user(user_id):
     user = Users.query.get(user_id)
     if user is None:
         return render_template('error.jinja2', error="User not found")
-    return render_template('show.jinja2', user=user)
+    return render_template('users/show.jinja2', user=user)
 
 
 @app.route('/users/<int:user_id>/edit')
-def users_edit(user_id):
+def render_user_edit(user_id):
     user = Users.query.get(user_id)
     if user is None:
         return render_template('error.jinja2', error="User not found")
     if user.image_url == DEFAULT_IMAGE_URL:
         user.image_url = ""
-    return render_template('edit.jinja2', user=user)
+    return render_template('users/edit.jinja2', user=user)
 
 
 @app.route('/users/<int:user_id>/edit', methods=["POST"])
-def users_update(user_id):
+def update_user(user_id):
     try:
         fn = request.form['first_name']
         ln = request.form['last_name']
@@ -97,7 +98,7 @@ def users_update(user_id):
 
 
 @app.route('/users/<int:user_id>/delete', methods=["POST"])
-def users_destroy(user_id):
+def delete_user(user_id):
     user = Users.query.get(user_id)
     if user is None:
         return render_template('error.jinja2', error="User not found")
@@ -106,3 +107,83 @@ def users_destroy(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+
+# POSTS ROUTES
+@app.route('/users/<int:user_id>/posts/new')
+def render_new_post(user_id):
+    user = Users.query.get(user_id)
+    if user is None:
+        return render_template('error.jinja2', error="User not found")
+    return render_template('posts/new.jinja2', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def create_new_post(user_id):
+    user = Users.query.get(user_id)
+    if user is None:
+        return render_template('error.jinja2', error="User not found")
+
+    try:
+        title = request.form['title']
+        content = request.form['content']
+    except KeyError:
+        return render_template('error.jinja2', error="Missing form data")
+
+    post = Posts(title=title, content=content, user=user)
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/users/{user_id}")
+
+
+@app.route('/posts/<int:post_id>')
+def render_post(post_id):
+    post = Posts.query.get(post_id)
+    if post is None:
+        return render_template('error.jinja2', error="Post not found")
+    return render_template('posts/show.jinja2', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def render_post_edit(post_id):
+    post = Posts.query.get(post_id)
+    if post is None:
+        return render_template('error.jinja2', error="Post not found")
+    return render_template('posts/edit.jinja2', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def update_post(post_id):
+    try:
+        title = request.form['title']
+        content = request.form['content']
+    except KeyError:
+        return render_template('error.jinja2', error="Missing form data")
+
+    post = Posts.query.get(post_id)
+    if post is None:
+        return render_template('error.jinja2', error="Post not found")
+
+    post.title = title
+    post.content = content
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/posts/{post_id}")
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def delete_post(post_id):
+    post = Posts.query.get(post_id)
+    if post is None:
+        return render_template('error.jinja2', error="Post not found")
+
+    user_id = post.user.id
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f"/users/{user_id}")
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 """Models for Blogly."""
+from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,3 +18,15 @@ class Users(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     image_url = db.Column(db.String(500), nullable=True)
+
+    posts = db.relationship('Posts', backref='user', cascade='all, delete-orphan')
+
+
+class Posts(db.Model):
+    __table_name__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
